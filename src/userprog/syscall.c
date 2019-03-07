60c589e4004ddd243f74ebfd632a6c7788889a7d
Lev Kavs <levnikolaj@WIN2.CS.KSU.EDU>
2019-03-07 15:36:48 -0600

    testing stuff, trying to get lev back to master branch

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,7 @@ static void syscall_handler (struct intr_frame *);
 
 struct lock file_system_lock;
 
-void
-syscall_init (void)
+void syscall_init (void)
 {
   lock_init(&file_system_lock);
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
